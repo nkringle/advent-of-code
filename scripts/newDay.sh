d23bb56f79c0249c8733779cba8d1ff6051ab372
Nick Aceto <naceto@sourceallies.com>
2022-12-02 10:04:37 -0600

    fix scripts to take year

--- scripts/newDay.sh
+++ scripts/newDay.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-mkdir src/day$1
+
+if [$1 == ""]; then
+  echo "You must provide a day"
+  exit 1
+fi
+
+if [$2 == ""]; then
+  echo "No year provided, defualting to current year"
+  YEAR=`date +%Y`
+else
+  YEAR=`$2`
+fi
+
+mkdir src/$YEAR/day$1
 
 if [ $? -ne 0 ] ; then
   echo "Day already exists!"
@@ -8,10 +21,10 @@ fi
 
 echo "export const day$1 = () => {
   return $1;
-};" >> src/day$1/day$1.ts
+};" >> src/$YEAR/day$1/day$1.ts
 
 echo "import { day$1 } from './day$1';
-import { logAnswer } from '../utils/logging';
+import { logAnswer } from '../../utils/logging';
 
 test('Provided test cases', () => {
   expect(day$1()).toBe($1);
@@ -21,11 +34,11 @@ test('Returns an answer', () => {
   logAnswer(day$1());
   expect(typeof day$1()).toBe('number');
   expect(day$1()).toBeGreaterThan(0);
-});" >> src/day$1/day$1.test.ts
+});" >> src/$YEAR/day$1/day$1.test.ts
 
-echo "import { parseInput } from '../utils/input';
+echo "import { parseInput } from '../../utils/input';
 
 const input = '';
 
-export const data = parseInput(input);" >> src/day$1/day$1.data.ts
+export const data = parseInput(input);" >> src/$YEAR/day$1/day$1.data.ts
 exit
--- scripts/partTwo.sh
+++ scripts/partTwo.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+if [$1 == ""]; then
+  echo "You must provide a day"
+  exit 1
+fi
+
+if [$2 == ""]; then
+  echo "No year provided, defualting to current year"
+  YEAR=`date +%Y`
+else
+  YEAR=`$2`
+fi
+
 echo "export const day$1part2 = () => {
   return $1;
-};" >> src/day$1/day$1.part2.ts
+};" >> src/$YEAR/day$1/day$1.part2.ts
 
 echo "import { day$1part2 } from './day$1.part2';
-import { logAnswer } from '../utils/logging';
+import { logAnswer } from '../../utils/logging';
 
 test('Provided test cases', () => {
   expect(day$1part2()).toBe($1);
@@ -14,4 +27,4 @@ test('Returns an answer', () => {
   logAnswer(day$1part2());
   expect(typeof day$1part2()).toBe('number');
   expect(day$1part2()).toBeGreaterThan(0);
-});" >> src/day$1/day$1.part2.test.ts
+});" >> src/$YEAR/day$1/day$1.part2.test.ts
