9b12ac2a852a1c55a17129b870297531b71943c8
Nick Aceto <nkringle@gmail.com>
2022-12-06 17:10:07 -0600

    day 5 part 2

--- src/2022/day5/day5.part2.test.ts
+++ src/2022/day5/day5.part2.test.ts
@@ -1,13 +1,25 @@
-import { day5part2 } from './day5.part2';
+import { calcAnswer, day5part2 } from './day5.part2';
 import { logAnswer } from '../../utils/logging';
 
-test('Provided test cases', () => {
-  expect(day5part2()).toBe(5);
+test('run test case', () => {
+  const answer = calcAnswer(`
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`);
+  
+
+  expect(answer).toEqual('MCD')
 });
 
 test('Returns an answer', () => {
-  const answer =day5part2();
+  const answer = day5part2();
   logAnswer(answer);
-  expect(typeof answer).toBe('number');
-  expect(answer).toBeGreaterThan(0);
+  expect(typeof answer).toBe('string');
+  expect(answer.length).toBeGreaterThan(0);
 });
--- src/2022/day5/day5.part2.ts
+++ src/2022/day5/day5.part2.ts
@@ -1,3 +1,27 @@
+import { parseCrates, parseMoves } from "./day5";
+import { input } from "./day5.data";
+
+export function calcAnswer(inp: string): string {
+  const crates: string[][] = parseCrates(inp);
+  const moves: object[] = parseMoves(inp);
+  
+  moves.forEach(move => {
+    const thisMove = crates[move.from - 1].slice( crates[move.from - 1].length - move.qty);
+
+    crates[move.to - 1 ].push(...thisMove);
+
+    for (let i = 0; i < move.qty; i++) {
+      crates[move.from - 1].pop();
+    }
+
+  });  
+  
+  return crates.map(column => {
+    return String(column.pop());
+  }).reduce((prev, cur) => prev?.concat(cur), ""); 
+}
+
+
 export const day5part2 = () => {
-  return 5;
+  return calcAnswer(input);
 };
