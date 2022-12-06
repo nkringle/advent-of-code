ed8c20a684d786748ad662d64857702c332da8fb
Nick Aceto <nkringle@gmail.com>
2022-12-05 19:25:23 -0600

    wip day 5

--- src/2022/day5/day5.test.ts
+++ src/2022/day5/day5.test.ts
@@ -1,9 +1,25 @@
-import { day5 } from './day5';
+import { calcAnswer, day5, parseCrates } from './day5';
 import { logAnswer } from '../../utils/logging';
 
-test('Provided test cases', () => {
-  expect(day5()).toBe(5);
+test('Parse Crates', () => {
+  const crates = parseCrates(`    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 `);
+
+  expect(crates.length).toBe(3); // columns
+  expect(crates[0]).toIncludeSameMembers(['N', 'Z']);
+  expect(crates[1]).toIncludeSameMembers(['D', 'C', 'M']);
+  expect(crates[2]).toIncludeSameMembers(['P']);
+
+
 });
+
+
+test('given full test cases', () => {
+  expect(calcAnswer(``)).toEqual(3);
+});
+
 
 test('Returns an answer', () => {
   const answer = day5();
--- src/2022/day5/day5.ts
+++ src/2022/day5/day5.ts
@@ -1,3 +1,31 @@
+import { input } from "./day5.data";
+
+
+export function parseCrates(inp: string): string[][] {
+  
+  const crateJunk = new RegExp('\[*\]*');
+
+  const cleaned = inp.replace(crateJunk, '').split('\n');
+  const crates = cleaned.filter((line) => !line.startsWith('move') && !line.startsWith(' 1'));
+  const columnsRow = cleaned
+    .map((line) => line.trim())
+    .filter((line) => line.startsWith('1'))[0];
+  const columns = Number(columnsRow.split('   ').pop());
+
+  const crateArrangement: string[][] = Array(columns).fill([]);
+
+  // todo need to get creates and put in list
+
+  return crateArrangement;
+}
+
+
+
+export function calcAnswer(inp: string): number {
+  return 0;
+}
+
+
 export const day5 = () => {
-  return 5;
+  return calcAnswer(input);
 };
